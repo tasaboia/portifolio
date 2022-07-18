63bb38d888282427684779f955172c85c3331025
Tainá Saboia <tasaboia@users.noreply.github.com>
2022-07-18 01:44:43 +0100

    responsive contact me

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,19 +1,27 @@
 import React from 'react'
-import { Button, FormControl, FormHelperText, InputLabel, Stack, TextField, Typography } from '@mui/material'
+import { Box, Button, FormControl, FormHelperText, InputLabel, Stack, TextField, Typography } from '@mui/material'
 
 export default function Contact() {
   
   return (
-    <Stack>
-      <Stack>
-        <Typography>
-          Entre em contato
-        </Typography>
-      </Stack>
-      <Stack>
-        <FormControl>
-          
-        </FormControl>
+    <Stack 
+      height='100vh'
+      display='flex' 
+    >
+      <Stack width='100%' alignItems='center' alignSelf='center'>
+        <Stack padding={10} textAlign='center'>
+          <Typography fontSize='3rem'>
+            Entre em contato
+          </Typography>
+        </Stack>
+        <Stack justifyItems='center' sx={{ width:{sx: '60vh', sm: '80vh'}}} >
+          <FormControl>
+          <TextField name='name' id="name" label="Digite seu nome" variant="standard" />
+          <TextField name='email' id="email" label="E-mail" variant="standard" />
+          <TextField name='message' id="msg" label="Mensagem" variant="standard" multiline rows={4} /> 
+          </FormControl>
+        </Stack>
+        
       </Stack>
     </Stack>
   ) 
