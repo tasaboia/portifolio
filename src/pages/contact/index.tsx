3b5a489ffe60c304db6457ee5dfa173698dea51d
Tainá Saboia <tasaboia@users.noreply.github.com>
2022-07-18 03:33:43 +0100

    added link nav bar to social media

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -17,16 +17,16 @@ export default function Contact() {
           </Typography>
         </Stack>
         <Stack justifyItems='center' sx={{ width: { sx: '60vh', sm: '80vh' } }}>
-          <FormControl>
+          <form name="contact" method="POST" data-netlify="true">
             <TextField name='name' id="name" label="Digite seu nome" variant="standard" />
-            <TextField name='email' id="email" label="E-mail" variant="standard" />
-            <TextField name='message' id="msg" label="Mensagem" variant="standard" multiline rows={4} />
+            <TextField name='email' type='email' id="email" label="E-mail" variant="standard" />
+            <TextField name="message" id="msg" label="Mensagem" variant="standard" multiline rows={4} />
             <Stack paddingTop={2} alignSelf='flex-end'>
               <Button type='submit' variant="outlined">
                 Enviar
               </Button>
             </Stack>
-          </FormControl>
+          </form>
         </Stack>
       </Stack>
     </Stack>
