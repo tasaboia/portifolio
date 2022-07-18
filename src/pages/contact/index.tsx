fa9202dbef006247f08c13ea0b9e0eb4daeed85a
Tainá Saboia <tasaboia@users.noreply.github.com>
2022-07-18 04:35:28 +0100

    project responsive fixed

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Button, FormControl, FormHelperText, InputLabel, Stack, TextField, Typography } from '@mui/material'
+import { Box, Button, FormControl, FormHelperText, InputLabel, Paper, Stack, TextField, Typography } from '@mui/material'
 import Footer from '../footer'
 
 export default function Contact() {
@@ -16,18 +16,23 @@ export default function Contact() {
             Entre em contato
           </Typography>
         </Stack>
-        <Stack justifyItems='center' sx={{ width: { sx: '60vh', sm: '80vh' } }}>
-          <FormControl>
-            <TextField name='name' id="name" label="Digite seu nome" variant="standard" />
-            <TextField name='email' id="email" label="E-mail" variant="standard" />
-            <TextField name='message' id="msg" label="Mensagem" variant="standard" multiline rows={4} />
-            <Stack paddingTop={2} alignSelf='flex-end'>
-              <Button type='submit' variant="outlined">
-                Enviar
-              </Button>
-            </Stack>
-          </FormControl>
-        </Stack>
+        
+        <Box padding={4} boxShadow={2} borderRadius={10} >
+
+          <Stack justifyItems='center' sx={{ width: { sx: '60vh', sm: '80vh' } }}>
+              <FormControl>
+                <TextField name='name' id="name" label="Digite seu nome" variant="standard" />
+                <TextField name='email' id="email" label="E-mail" variant="standard" />
+                <TextField name='message' id="msg" label="Mensagem" variant="standard" multiline rows={4} />
+                <Stack paddingTop={2} alignSelf='flex-end'>
+                  <Button type='submit' variant="outlined">
+                    Enviar
+                  </Button>
+                </Stack>
+              </FormControl>
+          </Stack>
+        </Box>
+
       </Stack>
     </Stack>
     <Footer />
