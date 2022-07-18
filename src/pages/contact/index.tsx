6b83b43fc5a3ebe9bf156f9fdba7bd04e0d2db75
Tainá Saboia <tasaboia@users.noreply.github.com>
2022-07-18 04:13:10 +0100

    project responsive fixed

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -17,17 +17,16 @@ export default function Contact() {
           </Typography>
         </Stack>
         <Stack justifyItems='center' sx={{ width: { sx: '60vh', sm: '80vh' } }}>
-          <form name="contact" method="POST" data-netlify="true" >
-            <input type="hidden" name="form-name" value="contact" />  
+          <FormControl>
             <TextField name='name' id="name" label="Digite seu nome" variant="standard" />
-            <TextField name='email' type='email' id="email" label="E-mail" variant="standard" />
-            <TextField name="message" id="msg" label="Mensagem" variant="standard" multiline rows={4} />
+            <TextField name='email' id="email" label="E-mail" variant="standard" />
+            <TextField name='message' id="msg" label="Mensagem" variant="standard" multiline rows={4} />
             <Stack paddingTop={2} alignSelf='flex-end'>
               <Button type='submit' variant="outlined">
                 Enviar
               </Button>
             </Stack>
-          </form>
+          </FormControl>
         </Stack>
       </Stack>
     </Stack>
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Card, CardActionArea, CardContent, CardMedia, Stack, Typography } from '@mui/material'
+import { Box, Card, CardActionArea, CardContent, CardMedia, Grid, Stack, Typography } from '@mui/material'
 import React from 'react'
 
 export default function Projects() {
@@ -14,42 +14,45 @@ export default function Projects() {
       >
         <Stack padding={10} textAlign='center'>
           <Typography fontSize='4rem'>Projetos</Typography>
-
         </Stack>
-        <Stack spacing={2} sx={{ display: {xs:'block', sm: 'flex'},  flexDirection:{xs:'column', sm: 'row'}}} alignSelf='center' justifyContent='center'>
-            <Card sx={{ maxWidth: 300 }}>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="200"
-                image={require('../../assets/images/coletiva-background-2.png')}
-                alt="green iguana"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  Importa Coletiva
-                </Typography>
-              
-              </CardContent>
-            </CardActionArea>
-          </Card>
-          <Card sx={{ maxWidth: 300 }}>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="200"
-                image={require('../../assets/images/app-background.png')}
-                alt="green iguana"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  Importa Coletiva
-                </Typography>
-                
-              </CardContent>
-            </CardActionArea>
-          </Card>
-      </Stack>
+          <Grid padding={5} container sx={{ flexDirection: {xs: 'column', sm: 'row'}}} rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} justifyContent='center'>
+            <Grid item>
+              <Card sx={{ maxWidth: 300 }}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="200"
+                    image={require('../../assets/images/coletiva-background-2.png')}
+                    alt="Importa Coleta"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Importa Coletiva
+                    </Typography>
+                  
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+            <Grid item>
+              <Card sx={{ maxWidth: 300 }}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="200"
+                    image={require('../../assets/images/app-background.png')}
+                    alt="RMCenter App"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Importa Coletiva
+                    </Typography>
+                    
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          </Grid>
       </Box>
     </Stack>
   </section>
