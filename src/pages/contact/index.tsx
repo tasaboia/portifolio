deb5b286ab69ac04c2d32152f96bc7bd3cdb5f57
Tainá Saboia <tasaboia@users.noreply.github.com>
2022-07-18 01:05:12 +0100

    deploy config

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,39 +1,20 @@
-import { Button, FormControl, FormHelperText, InputLabel, Stack, TextField, Typography } from '@mui/material'
 import React from 'react'
-import { useForm, ValidationError } from '@formspree/react';
+import { Button, FormControl, FormHelperText, InputLabel, Stack, TextField, Typography } from '@mui/material'
 
 export default function Contact() {
-  const [state, handleSubmit] = useForm("xrgjnbql");
-  if (state.succeeded) {
-      return <p>Thanks for joining!</p>;
-  }
+  
   return (
-      <form onSubmit={handleSubmit}>
-      <label htmlFor="email">
-        Email Address
-      </label>
-      <input
-        id="email"
-        type="email" 
-        name="email"
-      />
-      <ValidationError 
-        prefix="Email" 
-        field="email"
-        errors={state.errors}
-      />
-      <textarea
-        id="message"
-        name="message"
-      />
-      <ValidationError 
-        prefix="Message" 
-        field="message"
-        errors={state.errors}
-      />
-      <button type="submit" disabled={state.submitting}>
-        Submit
-      </button>
-    </form>
-  )
+    <Stack>
+      <Stack>
+        <Typography>
+          Entre em contato
+        </Typography>
+      </Stack>
+      <Stack>
+        <FormControl>
+          
+        </FormControl>
+      </Stack>
+    </Stack>
+  ) 
 }
